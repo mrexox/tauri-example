e91d9c5a84da3c87844185a9ff8fcfb69f4aaf73
Valentin Kiselev <mrexox@evilmartians.com>
2025-03-03 10:45:13 +0300

    feat: create window from the code

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,16 +1,33 @@
+use tauri::{webview::WebviewWindowBuilder, App};
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-  tauri::Builder::default()
-    .setup(|app| {
-      if cfg!(debug_assertions) {
-        app.handle().plugin(
-          tauri_plugin_log::Builder::default()
-            .level(log::LevelFilter::Info)
-            .build(),
-        )?;
-      }
-      Ok(())
-    })
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .plugin(tauri_plugin_fs::init())
+        .setup(|app| {
+            create_main_window(app)?;
+            if cfg!(debug_assertions) {
+                app.handle().plugin(
+                    tauri_plugin_log::Builder::default()
+                        .level(log::LevelFilter::Info)
+                        .build(),
+                )?;
+            }
+            Ok(())
+        })
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
+
+fn create_main_window(app: &mut App) -> tauri::Result<()> {
+    let _ = WebviewWindowBuilder::new(app, "main", Default::default())
+        .disable_drag_drop_handler()
+        .resizable(true)
+        .inner_size(1100.0, 800.0)
+        .focused(true)
+        .title("")
+        .min_inner_size(600.0, 400.0)
+        .build()?;
+
+    Ok(())
 }
